e5b083aa6b8ce0c7f743275dac1c86bc66d7a1ea
1303641515@qq.com <1303641515@qq.com>
2019-09-09 08:47:03 +0800

    提交

---
+++ src/main/java/com/wangyongle/util/DateUtil.java
@@ -0,0 +1,39 @@
+package com.wangyongle.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 日期工具类
+*/
+public class DateUtil {
+	/*
+	* 方法1：(5分)
+	* 给一个时间对象，返回该时间所在月的1日0时0分0秒。例如一个Date对象的值是2019-05-18 11:37:22
+	* 则返回的结果为2019-05-01 00:00:00
+	*/
+	public static Date getDateByInitMonth(Date src){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(src);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		return calendar.getTime();
+	}
+	/*
+	* 方法2：(5分)
+	* 给一个时间对象，返回该时间所在月的最后日23时59分59秒，需要考虑月大月小和二月特殊情况。
+	* 例如一个Date对象的值是2019-05-18 11:37:22，则返回的时间为2019-05-31 23:59:59
+	* 例如一个Date对象的值是2019-02-05 15:42:18，则返回的时间为2019-02-28 23:59:59
+	*/
+	public static Date getDateByFullMonth(Date src){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(src);
+		calendar.add(Calendar.MONTH, 1);
+		Date date = getDateByInitMonth(calendar.getTime());
+		calendar.setTime(date);
+		calendar.set(Calendar.SECOND, -1);
+		return calendar.getTime();
+	}
+}
